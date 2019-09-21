9b084404e5ff2a122ddd78fc198a6a3ad7bcb8ef
github username <github email address>
2019-09-21 10:25:55 -0500

    saving progress

--- into-the-void/src/Page.js
+++ into-the-void/src/Page.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 
 
-export default class Page{
+export default class Page extends Component{
+    render(){
+        return(
+            <div>
+                If you see this, it worked
+        </div>
+        )
+    }
 }
