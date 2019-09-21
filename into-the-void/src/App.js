ad98d1dc31f24ae88ad96e8494006e26241542a4
github username <github email address>
2019-09-21 10:20:56 -0500

    slight restructure

--- into-the-void/src/App.js
+++ into-the-void/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Input from './inputbox'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Input />
       </header>
     </div>
   );
